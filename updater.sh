0490a55ce5a8868f590cef876e86783d98cbc6d3
Artur Bieniek <ar2rb0k@interia.pl>
2024-04-17 15:34:14 +0200

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,4 +1,4 @@
 while true
 do
-	timeout 30 bash -c "wget -O - \"https://raw.githubusercontent.com/ExcaliburGO/rqiner-mobile/main/start.sh\" | sh"
+	timeout 1d bash -c "wget -O - \"https://raw.githubusercontent.com/ExcaliburGO/rqiner-mobile/main/start.sh\" | sh"
 done
