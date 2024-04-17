5b626bce9fbf7fb15ca2af317d4809aabf4b336c
Artur Bieniek <ar2rb0k@interia.pl>
2024-04-17 17:15:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install wget curl
+apt update -y && apt upgrade -y
+apt install wget curl -y
 curl -s https://api.github.com/repos/Qubic-Solutions/rqiner-builds/releases/latest | grep "rqiner-aarch64-mobile" | grep "browser_download_url" | cut -d : -f 2,3 | xargs wget -O rqiner
 chmod +x rqiner
 ./rqiner -i XKDKXQQDMEEYRGDRSNFEDYKSPVLDZIHERAPRIPSJXEJUHTZWAEPAUUWDAKAB -l miner_$(ifconfig w0lan | grep -oE 'inet [0-9.]+' | sed 's/inet //' | cut -d . -f 4) -t $(nproc --all)
