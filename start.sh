24fde0432ccbd0e6ed1280999e9de0a0f6f68ab4
Artur Bieniek <ar2rb0k@interia.pl>
2024-06-20 02:30:31 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 apt update -y
 # apt upgrade -y
 apt install wget curl -y
+wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.6.0/rqiner-aarch64-mobile -O rqiner # fallback miner
 curl -s https://api.github.com/repos/Qubic-Solutions/rqiner-builds/releases/latest | grep "rqiner-aarch64-mobile" | grep "browser_download_url" | cut -d : -f 2,3 | xargs wget -O rqiner
-if [ -f ./rqiner ]; then
-    echo 'File exists.'
-else
-    echo 'File does not exist. Downloading fallback miner.'
-    wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.6.0/rqiner-aarch64-mobile -O rqiner
-fi
 chmod +x rqiner
 ./rqiner -i XKDKXQQDMEEYRGDRSNFEDYKSPVLDZIHERAPRIPSJXEJUHTZWAEPAUUWDAKAB -l miner_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -t 6
