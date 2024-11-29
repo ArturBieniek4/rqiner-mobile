136df767770d697b6d13c58428baed0087ff5e33
Artur Bieniek <ar2rb0k@interia.pl>
2024-11-30 00:05:21 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ rm -rf rqiner-mobile
 git clone https://github.com/ArturBieniek4/rqiner-mobile
 cp rqiner-mobile/init.sh init.sh
 cd rqiner-mobile
-while true do
+while true
+do
     timeout 12h bash miner_manager.sh
 done
