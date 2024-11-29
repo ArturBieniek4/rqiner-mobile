90e4c90c69d32a2f844ff62ebb9dbaa3e0b160de
Artur Bieniek <ar2rb0k@interia.pl>
2024-11-29 23:27:07 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
-apt update -y
-# apt upgrade -y
-apt install wget curl -y
-wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.6.0/rqiner-aarch64-mobile -O rqiner # fallback miner
-curl -s https://api.github.com/repos/Qubic-Solutions/rqiner-builds/releases/latest | grep "rqiner-aarch64-mobile" | grep "browser_download_url" | cut -d : -f 2,3 | xargs wget -O rqiner
-chmod +x rqiner
-./rqiner -i XKDKXQQDMEEYRGDRSNFEDYKSPVLDZIHERAPRIPSJXEJUHTZWAEPAUUWDAKAB -l miner_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -t 6
+sshd -o PermitRootLogin=yes -p 8022
+wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v1.1.1/rqiner-aarch64 -O /usr/bin/rqiner # fallback miner
+curl -s https://api.github.com/repos/Qubic-Solutions/rqiner-builds/releases/latest | grep "rqiner-aarch64-mobile" | grep "browser_download_url" | cut -d : -f 2,3 | xargs wget -O /usr/bin/rqiner
+chmod +x /usr/bin/rqiner
+rqiner -i XKDKXQQDMEEYRGDRSNFEDYKSPVLDZIHERAPRIPSJXEJUHTZWAEPAUUWDAKAB -l aminer_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -t $(nproc --all) -c "bash ccminer.sh"
