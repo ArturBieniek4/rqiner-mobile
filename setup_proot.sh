f479ded998103812c33e6e26925dfd2f3e479d01
Artur Bieniek <ar2rb0k@interia.pl>
2024-11-29 23:41:06 +0100

    Update setup_proot.sh

--- setup_proot.sh
+++ setup_proot.sh
@@ -1,5 +1,5 @@
 apt update
-DEBIAN_FRONTEND=noninteractive apt -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget curl openssh-server
+DEBIAN_FRONTEND=noninteractive apt -y install libcurl4-openssl-dev libjansson libomp-dev git screen nano jq wget curl openssh-server
 
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
