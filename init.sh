30ad4aaeb04206eeadabb3eea9ab30ade821f180
Artur Bieniek <ar2rb0k@interia.pl>
2024-11-30 01:48:59 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ git clone https://github.com/ArturBieniek4/rqiner-mobile
 cp rqiner-mobile/init.sh init.sh
 cd rqiner-mobile
 termux-wake-lock
+sshd -o PermitRootLogin=yes -p 8022
 while true
 do
     timeout 12h bash miner_manager.sh
