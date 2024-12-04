bf8057d0edd5fb0b3c13d5bbf7e002ddcd7c9755
Artur Bieniek <ar2rb0k@interia.pl>
2024-12-04 15:59:37 +0100

    Bump fallback miner version

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 sshd -o PermitRootLogin=yes -p 8022
-wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v1.1.1/rqiner-aarch64 -O /usr/bin/rqiner # fallback miner
+wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v1.1.2-experimental/rqiner-aarch64 -O /usr/bin/rqiner # fallback miner
 curl -s https://api.github.com/repos/Qubic-Solutions/rqiner-builds/releases/latest | grep "rqiner-aarch64" | grep "browser_download_url" | cut -d : -f 2,3 | xargs wget -O /usr/bin/rqiner
 chmod +x /usr/bin/rqiner
 rqiner -i XKDKXQQDMEEYRGDRSNFEDYKSPVLDZIHERAPRIPSJXEJUHTZWAEPAUUWDAKAB -l aminer_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -t $(nproc --all) -c "ccminer -a verus -o stratum+tcp://verushash.auto.nicehash.com:9200 -u NHbPXhaHUy9hTB9eEMMypZNYymMqgwpzMubp.aminer_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -p hybrid -t $(nproc --all)"
