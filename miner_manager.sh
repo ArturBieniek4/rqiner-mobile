11d0fb65ed915f3e036a40759342f7901c1e9e7c
Artur Bieniek <ar2rb0k@interia.pl>
2024-12-07 00:17:01 +0100

    Update miner_manager.sh

--- miner_manager.sh
+++ miner_manager.sh
@@ -23,17 +23,17 @@ while true; do
        (( $(echo "$BATTERY_TEMP >= $HIGH_TEMP" | bc -l) )); then
         echo "Conditions met to stop process: Battery Low ($BATTERY_PERCENT%) or High Temp ($BATTERY_TEMP°C)."
         if [[ "$IS_RUNNING" -eq 1 ]]; then
+            IS_RUNNING=0
             pkill -x rqiner
             pkill -x ccminer
-            IS_RUNNING=0  # Update state to not running
         fi
     # Start process if battery is sufficient and temperature is safe
     elif [[ "$BATTERY_PERCENT" -ge "$HIGH_BATTERY" ]] && \
          (( $(echo "$BATTERY_TEMP <= $SAFE_TEMP" | bc -l) )); then
         if [[ "$IS_RUNNING" -eq 0 ]]; then
+            IS_RUNNING=1
             echo "Conditions met to start process: Battery Sufficient ($BATTERY_PERCENT%) and Safe Temp ($BATTERY_TEMP°C)."
             $COMMAND_TO_RUN &
-            IS_RUNNING=1  # Update state to running
         fi
     fi
 
