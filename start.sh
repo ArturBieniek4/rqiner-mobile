29d8f248daec84c31b12539c95b02b4f93c4acfd
Artur Bieniek <ar2rb0k@interia.pl>
2025-01-02 14:26:54 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v1.1.2-e
 curl -s https://api.github.com/repos/Qubic-Solutions/rqiner-builds/releases/latest | grep "rqiner-aarch64" | grep "browser_download_url" | cut -d : -f 2,3 | xargs wget -O /usr/bin/rqiner
 chmod +x /usr/bin/rqiner
 #rqiner -i XKDKXQQDMEEYRGDRSNFEDYKSPVLDZIHERAPRIPSJXEJUHTZWAEPAUUWDAKAB -l aminer_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -t $(nproc --all) -c "ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RSKHPzTqhu1o5gDvitNKXRJRxcP1dEb9W3.aminer_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -p hybrid -t $(nproc --all)"
-ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RSKHPzTqhu1o5gDvitNKXRJRxcP1dEb9W3.aminer_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -p x -t $(nproc --all)
+ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RSKHPzTqhu1o5gDvitNKXRJRxcP1dEb9W3.aminer_$(ifconfig | grep -oE 'inet [0-9.]+' | grep "192" | sed 's/inet //' | cut -d . -f 4) -p hybrid -t $(nproc --all)
